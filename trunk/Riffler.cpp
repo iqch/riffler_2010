9031d3409b36e75814d7bd271e06e29fef0aa405
iqcook <iqcook@68188fda-caff-f2c6-9a96-a25293646ba6>
2010-08-27 11:20:40 +0000

    Stable macro-based alpha. 21 method.

--- trunk/Riffler.cpp
+++ trunk/Riffler.cpp
@@ -6,7 +6,7 @@
 *	Riffler.h - RenderMan DSO Rif-filter for using python scripts
 *  for filtering. Filter realization source
 *
-*	Version: 0.1
+*	Version: 0.2
 *	Authors: Egor N. Chashchin                   
 *	Contact: iqcook@gmail.com 
 * 
@@ -19,67 +19,182 @@ template<class T> PyObject* Riffler<T>::m_object = NULL;
 
 template<class T> Riffler<T>::Riffler<T>() {};
 
+#define PARSE_CALLBACK(callback) _##callback##Func = PyObject_GetAttrString(object, #callback); \
+if(PyCallable_Check(_##callback##Func)) ##callback = &Riffler::_##callback; \
+else Py_XDECREF(_##callback##Func);
+
 template<class T> void Riffler<T>::Setup(PyObject* object)
 {
 	m_object = object;
-	_FrameBeginFunc = NULL;
 
 	if(object == NULL) return;
 
-	_FrameBeginFunc = PyObject_GetAttrString(object, "FrameBegin");
-	if(PyCallable_Check(_FrameBeginFunc)) FrameBegin = &Riffler::_FrameBegin;
-	else Py_XDECREF(_FrameBeginFunc);
+	PARSE_CALLBACK(FrameBegin)
+	//_FrameBeginFunc = PyObject_GetAttrString(object, "FrameBegin");
+	//#if(PyCallable_Check(_FrameBeginFunc)) FrameBegin = &Riffler::_FrameBegin;
+	//else Py_XDECREF(_FrameBeginFunc);
+
+	PARSE_CALLBACK(FrameEnd)
+	PARSE_CALLBACK(WorldBegin)
+	PARSE_CALLBACK(WorldEnd)
+
+	PARSE_CALLBACK(AttributeBegin);
+	PARSE_CALLBACK(AttributeEnd);
+
+	PARSE_CALLBACK(TransformBegin);
+	PARSE_CALLBACK(TransformEnd);
+
+	PARSE_CALLBACK(SolidEnd);
+
+	//PARSE_CALLBACK(ObjectBegin);
+	PARSE_CALLBACK(ObjectEnd);
+
+	PARSE_CALLBACK(MotionEnd);
+
+	PARSE_CALLBACK(Identity);
+
+	PARSE_CALLBACK(ReverseOrientation);
+
+	PARSE_CALLBACK(ArchiveEnd);
+
+	PARSE_CALLBACK(Else);
+	PARSE_CALLBACK(IfEnd);
+
+	PARSE_CALLBACK(ResourceBegin);
+	PARSE_CALLBACK(ResourceEnd);
+	PARSE_CALLBACK(EditEnd);
+	PARSE_CALLBACK(EditAttributeEnd);
+	PARSE_CALLBACK(EditWorldEnd);
 };
 
-template<class T>  PyObject* Riffler<T>::_FrameBeginFunc = NULL;
+#define DEFINE_CALLBACKFN(callback) template<class T>  PyObject* Riffler<T>::_##callback##Func = NULL;
 
+DEFINE_CALLBACKFN(FrameBegin)
+DEFINE_CALLBACKFN(FrameEnd)
+DEFINE_CALLBACKFN(WorldBegin)
+DEFINE_CALLBACKFN(WorldEnd)
+
+DEFINE_CALLBACKFN(AttributeBegin);
+DEFINE_CALLBACKFN(AttributeEnd);
+
+DEFINE_CALLBACKFN(TransformBegin);
+DEFINE_CALLBACKFN(TransformEnd);
+
+DEFINE_CALLBACKFN(SolidEnd);
+
+//DEFINE_CALLBACKFN(ObjectBegin);
+DEFINE_CALLBACKFN(ObjectEnd);
+
+DEFINE_CALLBACKFN(MotionEnd);
+
+DEFINE_CALLBACKFN(Identity);
+
+DEFINE_CALLBACKFN(ReverseOrientation);
+
+DEFINE_CALLBACKFN(ArchiveEnd);
+
+DEFINE_CALLBACKFN(Else);
+DEFINE_CALLBACKFN(IfEnd);
+
+DEFINE_CALLBACKFN(ResourceBegin);
+DEFINE_CALLBACKFN(ResourceEnd);
+DEFINE_CALLBACKFN(EditEnd);
+DEFINE_CALLBACKFN(EditAttributeEnd);
+DEFINE_CALLBACKFN(EditWorldEnd);
+
+#define CLEAN_CALLBACK(callback) Py_XDECREF(_##callback##Func);
 template<class T> Riffler<T>::~Riffler()
 {
-	Py_XDECREF(_FrameBeginFunc);
+	CLEAN_CALLBACK(FrameBegin)
+	CLEAN_CALLBACK(FrameEnd)
+	CLEAN_CALLBACK(WorldBegin)
+	CLEAN_CALLBACK(WorldEnd)
+
+	CLEAN_CALLBACK(AttributeBegin);
+	CLEAN_CALLBACK(AttributeEnd);
+
+	CLEAN_CALLBACK(TransformBegin);
+	CLEAN_CALLBACK(TransformEnd);
+
+	CLEAN_CALLBACK(SolidEnd);
+
+	//CLEAN_CALLBACK(ObjectBegin);
+	CLEAN_CALLBACK(ObjectEnd);
+
+	CLEAN_CALLBACK(MotionEnd);
+
+	CLEAN_CALLBACK(Identity);
+
+	CLEAN_CALLBACK(ReverseOrientation);
+
+	CLEAN_CALLBACK(ArchiveEnd);
+
+	CLEAN_CALLBACK(Else);
+	CLEAN_CALLBACK(IfEnd);
+
+	CLEAN_CALLBACK(ResourceBegin);
+	CLEAN_CALLBACK(ResourceEnd);
+	CLEAN_CALLBACK(EditEnd);
+	CLEAN_CALLBACK(EditAttributeEnd);
+	CLEAN_CALLBACK(EditWorldEnd);
+
 	Py_XDECREF(m_object);
 };
 
-//RtToken	Riffler::_Declare(char *name, char *declaration)
-//{
-//
-//};
+#define CALLBACKFN(callback) template<class T> RtVoid Riffler<T>::_##callback
 
-template<class T> RtVoid	Riffler<T>::_FrameBegin(RtInt frame)
+//template<class T> RtVoid	Riffler<T>::_FrameBegin
+CALLBACKFN(FrameBegin)(RtInt frame)
 {
-	PyObject* pArgs = PyTuple_New(2);
+	//PyObject* pArgs = PyTuple_New(2);
 
-	PyTuple_SetItem(pArgs, 0, m_object);
+	//PyTuple_SetItem(pArgs, 0, m_object);
 
-	PyObject* pValue = PyInt_FromLong(frame);
-	PyTuple_SetItem(pArgs, 1, pValue);
+	//PyObject* pValue = PyInt_FromLong(frame);
+	//PyTuple_SetItem(pArgs, 1, pValue);
 
-	PyObject* pResult = PyObject_CallObject(_FrameBeginFunc, pArgs);
-
+	//PyObject* pResult = PyObject_CallObject(_FrameBeginFunc, pArgs);
+	
+	PyObject* pResult = PyObject_CallFunction(_FrameBeginFunc, "i", frame);
 	Py_XDECREF(pResult);
 
-	Py_DECREF(pArgs);
-	Py_DECREF(pValue);
+	//Py_DECREF(pArgs);
+	//Py_DECREF(pValue);
 };
 
-/*RtVoid	Riffler::_FrameEnd()
-{
+#define FORWARD_PLAIN(callback) CALLBACKFN(callback)() { PyObject* pResult = PyObject_CallFunction(_##callback##Func, NULL); Py_XDECREF(pResult); };
+FORWARD_PLAIN(FrameEnd)
 
-};
+FORWARD_PLAIN(WorldBegin)
+FORWARD_PLAIN(WorldEnd)
 
-RtVoid	Riffler::_WorldBegin()
-{
+FORWARD_PLAIN(AttributeBegin);
+FORWARD_PLAIN(AttributeEnd);
 
-};
+FORWARD_PLAIN(TransformBegin);
+FORWARD_PLAIN(TransformEnd);
 
-RtVoid	Riffler::_WorldEnd()
-{
+FORWARD_PLAIN(SolidEnd);
 
-};
+//FORWARD_PLAIN(ObjectBegin);
+FORWARD_PLAIN(ObjectEnd);
 
-RtVoid	Riffler::_Clipping(RtFloat hither, RtFloat yon)
-{
+FORWARD_PLAIN(MotionEnd);
 
-};*/
+FORWARD_PLAIN(Identity);
+
+FORWARD_PLAIN(ReverseOrientation);
+
+FORWARD_PLAIN(ArchiveEnd);
+
+FORWARD_PLAIN(Else);
+FORWARD_PLAIN(IfEnd);
+
+FORWARD_PLAIN(ResourceBegin);
+FORWARD_PLAIN(ResourceEnd);
+FORWARD_PLAIN(EditEnd);
+FORWARD_PLAIN(EditAttributeEnd);
+FORWARD_PLAIN(EditWorldEnd);
 
 template class Riffler<bool>;
 template class Riffler<int>;
--- trunk/Riffler.h
+++ trunk/Riffler.h
@@ -6,7 +6,7 @@
 *	Riffler.h - RenderMan DSO Rif-filter for using python scripts
 *  for filtering. Filter realization header
 *
-*	Version: 0.1
+*	Version: 0.2
 *	Authors: Egor N. Chashchin                   
 *	Contact: iqcook@gmail.com 
 * 
@@ -19,6 +19,8 @@ public:
 	virtual void Setup(PyObject*) = 0;
 	virtual ~SetupLayer() {};
 };
+
+#define DECLARE_CALLBACK(callback) static PyObject* _##callback##Func; static RtVoid _##callback
 
 template<class T> 
 
@@ -35,149 +37,159 @@ private:
 	static PyObject* m_object;
 	T m_member;
 	// CATCHERS
-	//RtToken 	_Declare(char *name, char *declaration); // - little complex to startpoint
-	static RtVoid  	_FrameBegin(RtInt frame);
-	static PyObject* _FrameBeginFunc;
-	/*RtVoid  	_FrameEnd();
-	RtVoid  	_WorldBegin();
-	RtVoid  	_WorldEnd();
-	RtVoid  	_Clipping(RtFloat hither, RtFloat yon);
-	RtVoid  	_ClippingPlane(RtFloat Nx, RtFloat Ny, RtFloat Nz, RtFloat Px, RtFloat Py, RtFloat Pz);
-	RtVoid  	_CropWindow(RtFloat xmin, RtFloat xmax, RtFloat ymin, RtFloat ymax);
-	RtVoid  	_DepthOfField(RtFloat fstop, RtFloat focallength, RtFloat focaldistance);
-	RtVoid  	_Format(RtInt xres, RtInt yres, RtFloat aspect);
-	RtVoid  	_FrameAspectRatio(RtFloat aspect);
-	RtVoid  	_ProjectionV(RtToken name, 	RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_ScreenWindow(RtFloat left, RtFloat right, RtFloat top, RtFloat bot);
-	RtVoid  	_Shutter(RtFloat min, RtFloat max);
-	RtVoid  	_DisplayV(char *name, RtToken type, RtToken mode, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_Exposure(RtFloat gain, RtFloat gamma);
-	RtVoid  	_ImagerV(RtToken name, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_PixelFidelity(RtFloat variation); // OBSOLETE call: see RiPixelVariance
-	RtVoid  	_PixelFilter(RtFilterFunc filterFunc, RtFloat xwidth, RtFloat ywidth);
-	RtVoid  	_PixelSamples(RtFloat xsamples, RtFloat ysamples);
-	RtVoid  	_PixelVariance(RtFloat variation);
-	RtVoid  	_Quantize(RtToken type, RtInt one, RtInt min, RtInt max, RtFloat ampl);
-	RtVoid  	_HiderV(RtToken type, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_OptionV(RtToken name, RtInt, RtToken[], RtPointer[]);
 
-	RtVoid  	_AttributeBegin();
-	RtVoid  	_AttributeEnd();
-	RtVoid  	_AttributeV(RtToken name, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_Color(RtColor color);
-	RtVoid  	_Opacity(RtColor color);
-	RtVoid  	_TextureCoordinates(RtFloat s1, RtFloat t1, 	RtFloat s2, RtFloat t2, RtFloat s3, RtFloat t3, RtFloat s4, RtFloat t4);
-	RtVoid  	_Matte(RtBoolean onoff);
-	RtVoid  	_ShadingInterpolation(RtToken type);
-	RtVoid  	_ShadingRate(RtFloat size);
+	DECLARE_CALLBACK(FrameBegin)(RtInt frame);
+	DECLARE_CALLBACK(FrameEnd)();
 
-	RtLightHandle	_AreaLightSourceV(RtToken name, RtInt, RtToken[], RtPointer[]);
-	RtLightHandle	_LightSourceV(RtToken name, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_Illuminate(RtLightHandle light, RtBoolean onoff);
-	RtVoid  	_AtmosphereV(RtToken name, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_DisplacementV(RtToken name,	RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_ExteriorV(RtToken name, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_InteriorV(RtToken name, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_SurfaceV(RtToken name,	RtInt, RtToken[], RtPointer[]);
+	DECLARE_CALLBACK(WorldBegin)();
+	DECLARE_CALLBACK(WorldEnd)();
 
-	RtVoid  	_TransformBegin();
-	RtVoid  	_TransformEnd();
-	RtVoid  	_Bound(RtBound bound);
-	RtVoid  	_Detail(RtBound bound);
-	RtVoid  	_DetailRange(RtFloat minv, RtFloat lowtran, RtFloat uptran, RtFloat maxv);
-	RtVoid  	_RelativeDetail(RtFloat relativedetail);
-	RtVoid  	_GeometricApproximation(RtToken type, RtFloat value);
-	RtVoid  	_Orientation(RtToken orientation);
-	RtVoid  	_ReverseOrientation();
-	RtVoid  	_Sides(RtInt sides);
+	DECLARE_CALLBACK(AttributeBegin)();
+	DECLARE_CALLBACK(AttributeEnd)();
 
-	RtVoid  	_ConcatTransform(RtMatrix transform);
-	RtVoid  	_CoordSysTransform(RtToken space);
-	RtVoid  	_CoordinateSystem(RtToken space);
-	RtVoid	_ScopedCoordinateSystem(RtToken space);
+	DECLARE_CALLBACK(TransformBegin)();
+	DECLARE_CALLBACK(TransformEnd)();
 
-	RtVoid  	_Identity();
-	RtVoid  	_Perspective(RtFloat fov);
-	RtVoid  	_Rotate(RtFloat angle, RtFloat dx, RtFloat dy, RtFloat dz);
-	RtVoid  	_Scale(RtFloat sx, RtFloat sy, RtFloat sz);
-	RtVoid  	_Skew(RtFloat angle, RtFloat dx1, RtFloat dy1, RtFloat dz1, RtFloat dx2, RtFloat dy2, RtFloat dz2);
-	RtVoid  	_Transform(RtMatrix transform);
-	RtVoid  	_Translate(RtFloat dx, RtFloat dy, RtFloat dz);
+	//RtVoid  	_SolidBegin(RtToken operation);
+	DECLARE_CALLBACK(SolidEnd)();
 
-	RtVoid  	_Basis(RtBasis ubasis, RtInt ustep, RtBasis vbasis, RtInt vstep);
-	RtVoid  	_BlobbyV(RtInt nleaf, RtInt ninst, RtInt inst[], RtInt nflt, RtFloat flt[], RtInt nstr, RtToken str[], RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_ConeV(RtFloat height, RtFloat radius, RtFloat tmax, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_CurvesV(RtToken type, RtInt ncurves, RtInt nvert[], RtToken wrap, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_CylinderV(RtFloat rad, RtFloat zmin, RtFloat zmax, RtFloat tmax, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_DiskV(RtFloat height, RtFloat radius, RtFloat tmax,	RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_GeneralPolygonV(RtInt nloops, RtInt nverts[], RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_GeometryV(RtToken type, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_HyperboloidV(RtPoint point1, RtPoint point2, RtFloat tmax,	RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_NuPatchV(RtInt nu, RtInt uorder, RtFloat uknot[], RtFloat umin, RtFloat umax, RtInt nv, RtInt vorder, RtFloat vknot[], RtFloat vmin, RtFloat vmax, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_ParaboloidV(RtFloat rmax, RtFloat zmin, RtFloat zmax, RtFloat tmax, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_PatchMeshV(RtToken type, RtInt nu, RtToken uwrap, RtInt nv, RtToken vwrap, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_PatchV(RtToken type, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_PointsGeneralPolygonsV(RtInt npolys, RtInt nloops[], RtInt nverts[], RtInt verts[], RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_PointsPolygonsV(RtInt npolys, RtInt nverts[], RtInt verts[], RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_PointsV(RtInt nverts, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_PolygonV(RtInt nverts, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_Procedural(RtPointer data, RtBound bound, RtProcSubdivFunc sdfunc, RtProcFreeFunc freefunc);
-	RtVoid  	_SphereV(RtFloat radius, RtFloat zmin, RtFloat zmax, RtFloat tmax, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_SubdivisionMeshV(RtToken mask, RtInt nf, RtInt nverts[], RtInt verts[], RtInt nt, RtToken tags[], RtInt nargs[], RtInt intargs[], RtFloat floatargs[], RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_TorusV(RtFloat majrad, RtFloat minrad, RtFloat phimin, RtFloat phimax, 	RtFloat tmax, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_TrimCurve(RtInt nloops, RtInt ncurves[], RtInt order[], RtFloat knot[], RtFloat min[], RtFloat max[], RtInt n[], RtFloat u[], RtFloat v[], RtFloat w[]);
+	//DECLARE_CALLBACK(ObjectBegin)();
+	DECLARE_CALLBACK(ObjectEnd)();
+	//RtVoid  	_ObjectInstance(RtObjectHandle handle);
 
-	RtVoid  	_SolidBegin(RtToken operation);
-	RtVoid  	_SolidEnd();
+	//RtVoid  	_MotionBeginV(RtInt n, RtFloat times[]);
+	DECLARE_CALLBACK(MotionEnd)();
 
-	RtObjectHandle  _ObjectBegin();
-	RtVoid	    	_ObjectEnd();
-	RtVoid  	_ObjectInstance(RtObjectHandle handle);
-
-	RtVoid  	_MotionBeginV(RtInt n, RtFloat times[]);
-	RtVoid  	_MotionEnd();
-
-	RtVoid  	_ResourceV(RtToken handle, RtToken type, RtInt, RtToken[], RtPointer[]);
-
-	RtVoid  	_MakeCubeFaceEnvironmentV(char *px, char *nx, char *py, char *ny,	char *pz, char *nz, char *tex, RtFloat fov,	RtFilterFunc filterFunc,	RtFloat swidth, RtFloat ywidth, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_MakeLatLongEnvironmentV(char *pic, char *tex, RtFilterFunc filterFunc,	RtFloat swidth, RtFloat twidth, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_MakeShadowV(char *pic, char *tex, 	RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_MakeTextureV(char *pic, char *tex, 	RtToken swrap, RtToken twrap,	RtFilterFunc filterFunc, RtFloat swidth, RtFloat twidth, RtInt, RtToken[], RtPointer[]);
-
-	RtVoid  	_VArchiveRecord(RtToken type, char *format, va_list vap);
-	RtVoid  	_ReadArchiveV(RtToken name, RtArchiveCallback callback, RtInt, RtToken[], RtPointer[]);
-
-	RtArchiveHandle _ArchiveBeginV(RtToken name, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_ArchiveEnd();
-
-	RtVoid  	_IfBeginV(char *expr, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_ElseIfV(char *expr, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_Else();
-	RtVoid  	_IfEnd();
-
-	RtVoid	_DisplayChannelV(RtToken mode, RtInt, RtToken[], RtPointer[]);
-
-	RtVoid	_MakeBrickMapV(RtInt, RtToken *, RtToken, RtInt, RtToken[], RtPointer[]);
+	DECLARE_CALLBACK(Identity)();
 	
-	RtVoid  	_HierarchicalSubdivisionMeshV(RtToken mask, RtInt nf, 	RtInt nverts[], RtInt verts[], RtInt nt, RtToken tags[],	RtInt nargs[], RtInt intargs[], RtFloat floatargs[], RtToken stringargs[], RtInt, RtToken[], RtPointer[]);
+	DECLARE_CALLBACK(ReverseOrientation)();
 
-	RtVoid  	_ResourceBegin();
-	RtVoid  	_ResourceEnd();
-	RtVoid	_System(char *cmdstring);
+	//RtArchiveHandle _ArchiveBeginV(RtToken name, RtInt, RtToken[], RtPointer[]);
+	DECLARE_CALLBACK(ArchiveEnd)();
 
-	RtVoid  	_ShaderV(RtToken name, RtToken handle, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_IfBeginV(char *expr, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_ElseIfV(char *expr, RtInt, RtToken[], RtPointer[]);
+	DECLARE_CALLBACK(Else)();
+	DECLARE_CALLBACK(IfEnd)();
 
-	RtVoid  	_CameraV(RtToken name, RtInt, RtToken[], RtPointer[]);
 
-	RtVoid	_ErrorHandler(RtErrorHandler);
+	DECLARE_CALLBACK(ResourceBegin)();
+	DECLARE_CALLBACK(ResourceEnd)();
+	//RtVoid  	_EditBeginV(RtToken name, RtInt, RtToken[], RtPointer[]);
+	DECLARE_CALLBACK(EditEnd)();
+	//EditAttributeBegin(RtToken name);
+	DECLARE_CALLBACK(EditAttributeEnd)();
+	//RtVoid	_EditWorldBeginV(RtToken name, RtInt, RtToken[], RtPointer[]);
+	DECLARE_CALLBACK(EditWorldEnd)();
 
-	RtVoid  	_EditBeginV(RtToken name, RtInt, RtToken[], RtPointer[]);
-	RtVoid  	_EditEnd();
-	RtVoid  	_EditAttributeBegin(RtToken name);
-	RtVoid  	_EditAttributeEnd();
-	RtVoid	_EditWorldBeginV(RtToken name, RtInt, RtToken[], RtPointer[]);
-	RtVoid	_EditWorldEnd();
+	//RtToken 	_Declare(char *name, char *declaration); // - little complex to startpoint
+	//RtVoid  	_Clipping(RtFloat hither, RtFloat yon);
+	//RtVoid  	_ClippingPlane(RtFloat Nx, RtFloat Ny, RtFloat Nz, RtFloat Px, RtFloat Py, RtFloat Pz);
+	//RtVoid  	_CropWindow(RtFloat xmin, RtFloat xmax, RtFloat ymin, RtFloat ymax);
+	//RtVoid  	_DepthOfField(RtFloat fstop, RtFloat focallength, RtFloat focaldistance);
+	//RtVoid  	_Format(RtInt xres, RtInt yres, RtFloat aspect);
+	//RtVoid  	_FrameAspectRatio(RtFloat aspect);
+	//RtVoid  	_ProjectionV(RtToken name, 	RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_ScreenWindow(RtFloat left, RtFloat right, RtFloat top, RtFloat bot);
+	//RtVoid  	_Shutter(RtFloat min, RtFloat max);
+	//RtVoid  	_DisplayV(char *name, RtToken type, RtToken mode, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_Exposure(RtFloat gain, RtFloat gamma);
+	//RtVoid  	_ImagerV(RtToken name, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_PixelFidelity(RtFloat variation); // OBSOLETE call: see RiPixelVariance
+	//RtVoid  	_PixelFilter(RtFilterFunc filterFunc, RtFloat xwidth, RtFloat ywidth);
+	//RtVoid  	_PixelSamples(RtFloat xsamples, RtFloat ysamples);
+	//RtVoid  	_PixelVariance(RtFloat variation);
+	//RtVoid  	_Quantize(RtToken type, RtInt one, RtInt min, RtInt max, RtFloat ampl);
+	//RtVoid  	_HiderV(RtToken type, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_OptionV(RtToken name, RtInt, RtToken[], RtPointer[]);
 
-	RtVoid  	_PixelSampleImagerV(RtToken name, RtInt, RtToken[], RtPointer[]);*/
+	//RtVoid  	_AttributeV(RtToken name, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_Color(RtColor color);
+	//RtVoid  	_Opacity(RtColor color);
+	//RtVoid  	_TextureCoordinates(RtFloat s1, RtFloat t1, 	RtFloat s2, RtFloat t2, RtFloat s3, RtFloat t3, RtFloat s4, RtFloat t4);
+	//RtVoid  	_Matte(RtBoolean onoff);
+	//RtVoid  	_ShadingInterpolation(RtToken type);
+	//RtVoid  	_ShadingRate(RtFloat size);
+
+	//RtLightHandle	_AreaLightSourceV(RtToken name, RtInt, RtToken[], RtPointer[]);
+	//RtLightHandle	_LightSourceV(RtToken name, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_Illuminate(RtLightHandle light, RtBoolean onoff);
+	//RtVoid  	_AtmosphereV(RtToken name, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_DisplacementV(RtToken name,	RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_ExteriorV(RtToken name, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_InteriorV(RtToken name, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_SurfaceV(RtToken name,	RtInt, RtToken[], RtPointer[]);
+
+	//RtVoid  	_Bound(RtBound bound);
+	//RtVoid  	_Detail(RtBound bound);
+	//RtVoid  	_DetailRange(RtFloat minv, RtFloat lowtran, RtFloat uptran, RtFloat maxv);
+	//RtVoid  	_RelativeDetail(RtFloat relativedetail);
+	//RtVoid  	_GeometricApproximation(RtToken type, RtFloat value);
+	//RtVoid  	_Orientation(RtToken orientation);
+	//RtVoid  	_Sides(RtInt sides);
+
+	//RtVoid  	_ConcatTransform(RtMatrix transform);
+	//RtVoid  	_CoordSysTransform(RtToken space);
+	//RtVoid  	_CoordinateSystem(RtToken space);
+	//RtVoid	_ScopedCoordinateSystem(RtToken space);
+
+	
+	//RtVoid  	_Perspective(RtFloat fov);
+	//RtVoid  	_Rotate(RtFloat angle, RtFloat dx, RtFloat dy, RtFloat dz);
+	//RtVoid  	_Scale(RtFloat sx, RtFloat sy, RtFloat sz);
+	//RtVoid  	_Skew(RtFloat angle, RtFloat dx1, RtFloat dy1, RtFloat dz1, RtFloat dx2, RtFloat dy2, RtFloat dz2);
+	//RtVoid  	_Transform(RtMatrix transform);
+	//RtVoid  	_Translate(RtFloat dx, RtFloat dy, RtFloat dz);
+
+	//RtVoid  	_Basis(RtBasis ubasis, RtInt ustep, RtBasis vbasis, RtInt vstep);
+	//RtVoid  	_BlobbyV(RtInt nleaf, RtInt ninst, RtInt inst[], RtInt nflt, RtFloat flt[], RtInt nstr, RtToken str[], RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_ConeV(RtFloat height, RtFloat radius, RtFloat tmax, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_CurvesV(RtToken type, RtInt ncurves, RtInt nvert[], RtToken wrap, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_CylinderV(RtFloat rad, RtFloat zmin, RtFloat zmax, RtFloat tmax, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_DiskV(RtFloat height, RtFloat radius, RtFloat tmax,	RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_GeneralPolygonV(RtInt nloops, RtInt nverts[], RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_GeometryV(RtToken type, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_HyperboloidV(RtPoint point1, RtPoint point2, RtFloat tmax,	RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_NuPatchV(RtInt nu, RtInt uorder, RtFloat uknot[], RtFloat umin, RtFloat umax, RtInt nv, RtInt vorder, RtFloat vknot[], RtFloat vmin, RtFloat vmax, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_ParaboloidV(RtFloat rmax, RtFloat zmin, RtFloat zmax, RtFloat tmax, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_PatchMeshV(RtToken type, RtInt nu, RtToken uwrap, RtInt nv, RtToken vwrap, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_PatchV(RtToken type, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_PointsGeneralPolygonsV(RtInt npolys, RtInt nloops[], RtInt nverts[], RtInt verts[], RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_PointsPolygonsV(RtInt npolys, RtInt nverts[], RtInt verts[], RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_PointsV(RtInt nverts, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_PolygonV(RtInt nverts, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_Procedural(RtPointer data, RtBound bound, RtProcSubdivFunc sdfunc, RtProcFreeFunc freefunc);
+	//RtVoid  	_SphereV(RtFloat radius, RtFloat zmin, RtFloat zmax, RtFloat tmax, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_SubdivisionMeshV(RtToken mask, RtInt nf, RtInt nverts[], RtInt verts[], RtInt nt, RtToken tags[], RtInt nargs[], RtInt intargs[], RtFloat floatargs[], RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_TorusV(RtFloat majrad, RtFloat minrad, RtFloat phimin, RtFloat phimax, 	RtFloat tmax, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_TrimCurve(RtInt nloops, RtInt ncurves[], RtInt order[], RtFloat knot[], RtFloat min[], RtFloat max[], RtInt n[], RtFloat u[], RtFloat v[], RtFloat w[]);
+
+
+	//RtVoid  	_ResourceV(RtToken handle, RtToken type, RtInt, RtToken[], RtPointer[]);
+
+	//RtVoid  	_MakeCubeFaceEnvironmentV(char *px, char *nx, char *py, char *ny,	char *pz, char *nz, char *tex, RtFloat fov,	RtFilterFunc filterFunc,	RtFloat swidth, RtFloat ywidth, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_MakeLatLongEnvironmentV(char *pic, char *tex, RtFilterFunc filterFunc,	RtFloat swidth, RtFloat twidth, RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_MakeShadowV(char *pic, char *tex, 	RtInt, RtToken[], RtPointer[]);
+	//RtVoid  	_MakeTextureV(char *pic, char *tex, 	RtToken swrap, RtToken twrap,	RtFilterFunc filterFunc, RtFloat swidth, RtFloat twidth, RtInt, RtToken[], RtPointer[]);
+
+	//RtVoid  	_VArchiveRecord(RtToken type, char *format, va_list vap);
+	//RtVoid  	_ReadArchiveV(RtToken name, RtArchiveCallback callback, RtInt, RtToken[], RtPointer[]);
+
+
+	//RtVoid	_DisplayChannelV(RtToken mode, RtInt, RtToken[], RtPointer[]);
+
+	//RtVoid	_MakeBrickMapV(RtInt, RtToken *, RtToken, RtInt, RtToken[], RtPointer[]);
+	//
+	//RtVoid  	_HierarchicalSubdivisionMeshV(RtToken mask, RtInt nf, 	RtInt nverts[], RtInt verts[], RtInt nt, RtToken tags[],	RtInt nargs[], RtInt intargs[], RtFloat floatargs[], RtToken stringargs[], RtInt, RtToken[], RtPointer[]);
+
+	//RtVoid	_System(char *cmdstring);
+
+	//RtVoid  	_ShaderV(RtToken name, RtToken handle, RtInt, RtToken[], RtPointer[]);
+
+	//RtVoid  	_CameraV(RtToken name, RtInt, RtToken[], RtPointer[]);
+
+	//RtVoid	_ErrorHandler(RtErrorHandler);
+
+	//RtVoid  	_PixelSampleImagerV(RtToken name, RtInt, RtToken[], RtPointer[]);
 };
 
